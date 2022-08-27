935acfc197b8ae1bb89396a03864b1232e75f494
ThatSGiB <100253035+ThatSGiB@users.noreply.github.com>
2022-08-27 19:21:23 -0400

    Update
    I still don't know what I'm doing but I'm doing it really well

---
+++ PE1/Betze_HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿//Spencer Betze, PE1: Hello World, 8/24/2022
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betze_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Had the console write my name like in the instructions
+            Console.WriteLine("Spencer Betze");
+            //created variables using var, it seems that let does not work in this
+            var x = 4;
+            var y = 6;
+            //Console prints the answer of the equation, and prints exactly what is written in quotes, regardless of what it once was
+            Console.WriteLine(x + y);
+            Console.WriteLine("x + y");
+            //If and for statements seem to work exactly the same as in JS, using int instead of let
+            if (x == 4)
+            {
+                for(int i= 0; i<= y; i++)
+                {
+                    Console.WriteLine(i * (x + y));
+                }
+            }
+        }
+    }
+}
---
+++ PE2/SquashTheBugs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SquashTheBugs
+{
+    // Class Program
+    // Author: David Schuh
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10 
+        //          Output N/(N-1) for all 10 numbers
+        //          and list all numbers processed
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare int counter
+            //int i = 0
+            //Didn't have ;, and doesn't need to equal anything yet, as it will be issued a value later
+            int i;
+            string allNumbers = null;
+
+            // loop through the numbers 1 through 10
+            for (i = 1; i < 10; ++i)
+            {
+                // declare string to hold all numbers
+                //string allNumbers = null;
+                //Was in the wrong location, needed to be before the loop in order for it to be used outside of the loop
+
+                // output explanation of calculation
+                //Console.Write(i + "/" + i - 1 + " = ");
+                //Was attempting to combine strings and integers, so put "" around -1 so that it wouldn't try to do that
+                Console.Write(i + "/" + i + "- 1" + " = ");
+
+                // output the calculation based on the numbers
+                //Console.WriteLine(i / (i - 1));
+                //Created an if statement to skip this calculation running 1 because it would otherwise divide by 0
+                if (i == 1)
+                { } 
+                else if(i > 1)
+                { Console.WriteLine(i / (i - 1)); }
+                
+
+                // concatenate each number to allNumbers
+                allNumbers += i + " ";
+
+                // increment the counter
+                i = i + 1;
+            }
+
+            // output all numbers which have been processed
+            Console.WriteLine("These numbers have been processed: ", allNumbers);
+        }
+    }
+}
