127d9b5be88172953a5767717c041cc99d37363f
ThatSGiB <100253035+ThatSGiB@users.noreply.github.com>
2022-09-30 23:21:06 -0400

    update
    added latest assignments

---
+++ PE11/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE11
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger()
+        {
+
+        }
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+    }
+
+    public abstract class Compact : Car, IPassengerCarrier
+    {
+
+    }
+
+    public abstract class SUV : Car, IPassengerCarrier
+    {
+
+    }
+    public abstract class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+    public abstract class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+    public abstract class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public abstract class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+}
