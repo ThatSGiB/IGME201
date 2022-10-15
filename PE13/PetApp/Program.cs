630f1244522379e295447b80872dba0e53acb7c6
ThatSGiB <100253035+ThatSGiB@users.noreply.github.com>
2022-10-14 23:26:01 -0400

    PE16
    added PE16

---
+++ PE13/PetApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp
+{
+    public interface ICat
+    {
+        void Eat()
+        {
+
+        }
+        void Play()
+        {
+
+        }
+        void Scracth()
+        {
+
+        }
+        void Purr()
+        {
+
+        }
+    }
+
+    public class Cat : Pet, ICat
+    {
+        public void Eat()
+        {
+
+        }
+        public void Play()
+        {
+
+        }
+        public void Purr()
+        {
+
+        }
+        public void Scratch()
+        {
+
+        }
+        public void GotoVet()
+        {
+
+        }
+    }
+    public abstract class Pet
+    {
+        public string name;
+        public int age;
+        public string Name;
+        public abstract void Eat();
+        public abstract void Play();
+        public abstract void GotoVet();
+    }
+
+    public class Dog: Pet
+    {
+        public string license;
+        public void Eat();
+        public void Play();
+        public void Bark();
+        public void NeedWalk();
+        public void GotoVet();
+    }
+}
---
+++ PE16/CafeLib/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeLib
+{
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        byte sugar;
+        public string customer;
+        public byte amount;
+        public void AddSugar(byte amount)
+        {
+
+        }
+
+        public void Steam()
+        {
+
+        }
+    }
+
+    public interface IMood
+    {
+        public string Mood;
+    }
+
+    public interface ITakeOrder
+    {
+        public void TakeOrder()
+        {
+
+        }
+    }
+
+    public class Waiter: IMood
+    {
+        public string name;
+
+        public void ServeCustomer()
+        {
+            
+        }
+
+    }
+
+    public class Customer: IMood
+    {
+        public string name;
+        public string creditCardNumber;
+    }
+
+    public class CupOfCoffee: HotDrink, ITakeOrder
+    {
+        public string beanType;
+
+        public Object Steam()
+        {
+            return beanType;
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+    }
+
+    public class CupOfTea: HotDrink, ITakeOrder
+    {
+        public string leafType;
+
+        public Object Steam()
+        {
+            return leafType;
+        }
+
+        public void TakeOrder()
+        {
+            
+        }
+
+        public bool customerIsWealthy;
+    }
+
+    public class CupOfCocoa: HotDrink, ITakeOrder
+    {
+        public int numCups;
+        public bool marshmallows;
+        string source;
+
+        public string Source;
+        public Object Steam()
+        {
+            return source;
+        }
+
+        
+
+        public Object AddSugar()
+        {
+            
+            return HotDrink.amount;
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+    }
+}
