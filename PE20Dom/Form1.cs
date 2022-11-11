b8d57685fd4c4b35e58d84550d4ec8e714b194bb
ThatSGiB <100253035+ThatSGiB@users.noreply.github.com>
2022-11-10 23:38:37 -0500

    PE20
    Added latest assignment

---
+++ PE20Dom/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PE20Dom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
+                    @"SOFTWARE\\WOW6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION",
+                    true);
+                key.SetValue(Application.ExecutablePath.Replace(Application.StartupPath + "\\", ""), 12001, Microsoft.Win32.RegistryValueKind.DWord);
+                key.Close();
+            }
+            catch
+            {
+
+            }
+
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.WebBrowser1__DocumentCompleted);
+
+            this.webBrowser1.Navigate("people.rit.edu/dxsigm/example.html");
+
+        }
+
+        private void WebBrowser1__DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser webBrowser = (WebBrowser)sender;
+            HtmlElementCollection htmlElementCollection;
+            HtmlElement htmlElement;
+
+            htmlElementCollection = webBrowser.Document.GetElementsByTagName("h1");
+            htmlElement = htmlElementCollection[0];
+            htmlElement.InnerText = "My UFO Page";
+
+            htmlElementCollection = webBrowser.Document.GetElementsByTagName("h2");
+            htmlElementCollection[0].InnerText = "My UFO Info";
+            htmlElementCollection[1].InnerText = "My UFO Pictures";
+            htmlElementCollection[2].InnerText = null;
+
+            htmlElement = webBrowser.Document.Body;
+            htmlElement.Style = "color: #a60000; font-family: sans-serif;";
+
+            htmlElementCollection = webBrowser.Document.GetElementsByTagName("p");
+            htmlElementCollection[0].InnerHtml = "Report your UFO sightigns here: <a href='http://www.nuforc.org'>http://www.nuforc.org</a>";
+            htmlElementCollection[0].Style = "color: green; font-weight: bold; font-size: 2em; text-transform: uppercase; text-shadow: 3px 2px #A44";
+            htmlElementCollection[1].InnerText = null;
+            htmlElementCollection[2].InnerHtml = "<img src=https://i.kym-cdn.com/photos/images/original/002/017/009/c38.png alt=woah a ufo>";
+
+            htmlElement = webBrowser.Document.CreateElement("footer");
+            htmlElement.InnerHtml = "&copy; 2022 Spencer Betze";
+            webBrowser.Document.Body.AppendChild(htmlElement);
+
+            //This is really weird because we just did the exact same assignment in my 235 class but doing it with javascript instead, it's very bizarre learning how to do the exact same thing in two languages at the same time
+        }
+
+    }
+}
